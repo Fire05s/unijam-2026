b478443ed074e705d65280a12929390b2c221f29
sprigdonut <chenk27@uci.edu>
2026-07-11 01:39:32 -0700

    dinoparts.cs AGAIN

---
+++ Assets/Scripts/Items/dinoparts.cs
@@ -0,0 +1,42 @@
+struct DinosaurPart
+{
+    BodyPartType type;
+    List<Stat> stats;
+}
+
+struct Stat
+{
+    Stat(string a, float v)
+    {
+        type = stringtostat(a); value = v;
+    }
+    StatType type;
+    float value;
+
+    public static string stattostring(StatType s)
+    {
+        switch(s)
+        {
+            case StatType.Attack: return "Attack";
+            case StatType.Speed: return "Speed";
+            case StatType.Health: return "Health";
+            case StatType.CritDamage: return "Critical Hit Damage";
+            case StatType.CritRate: return "Critical Hit Rate";
+        }
+    }
+    public static StatType stringtostat(string s)
+    {
+        switch(s)
+        {
+            case "Attack": return StatType.Attack;
+            case "Speed": return StatType.Speed;
+            case "Health": return StatType.Health;
+            case "Critical Hit Damage": return StatType.CritDamage;
+            case "Critical Hit Rate": return StatType.CritRate;
+        }
+    }
+}
+enum StatType
+{
+    Attack,Speed,Health,CritRate,CritDamage
+}
---
+++ Assets/Scripts/UI/swapScreen.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+class SwapScreen
+{
+    SwapScreen(BodyPartSO currentPart, List<BodyPartSO> inventory)
+    {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
