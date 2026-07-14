fd9c118f33c05125541b2cbd21a78cadbfae42f2
Brandon Tsai <btsai77@gmail.com>
2026-07-13 20:41:32 -0700

    tweaks to sightline script

--- Assets/Scripts/Dinosaurs/enemySightlines.cs
+++ Assets/Scripts/Dinosaurs/enemySightlines.cs
@@ -1,58 +1,47 @@
+using Combat;
 using UnityEngine;
 
-public class enemySightlines : MonoBehaviour
+public class EnemySightlines : MonoBehaviour
 {
     [Header("Sightline")]
-    public Transform sightLineOrigin;
-    public float sightLineDistance;
+    [SerializeField] private Transform _sightLineOrigin;
+    [SerializeField] private float _sightLineDistance;
     [Header("Scene Transition")]
-    public GameObject transitionObject;
-    public string battleScene;
-    public float transitionDuration;
+    [SerializeField] private GameObject _transitionObject;
+    [SerializeField] private string _battleScene;
+    [SerializeField] private float _transitionDuration;
+    [Header("Associated Battle")]
+    [SerializeField] private BattleData _battleData;
 
-
-    private LineRenderer laserLine;
-    private bool hasLineOfSight;
-    private GameObject player;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-        laserLine = GetComponent<LineRenderer>();
-    }
-    LayerMask layerMask;
+    private LayerMask layerMask;
     void Awake()
     {
         layerMask = LayerMask.GetMask("Wall", "Default");
     }
-    // Update is called once per frame
-    void Update()
+
+    private void OnDrawGizmos()
     {
-        
+        if (_sightLineOrigin == null)
+        {
+            return;
+        }
+
+        Gizmos.color = Color.red;
+        Vector3 direction = transform.TransformDirection(Vector3.forward);
+        Gizmos.DrawRay(_sightLineOrigin.position, direction * _sightLineDistance);
     }
+
     private void FixedUpdate()
     {
         RaycastHit hit;
-        if (Physics.Raycast(sightLineOrigin.transform.position, transform.TransformDirection(Vector3.forward), out hit, sightLineDistance, layerMask))
+        if (Physics.Raycast(_sightLineOrigin.transform.position, transform.TransformDirection(Vector3.forward), out hit, _sightLineDistance, layerMask))
         {
-            if(hit.collider.CompareTag("Player"))
+            if(hit.transform.gameObject.CompareTag("Player"))
             {
-                Debug.DrawRay(sightLineOrigin.transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
-                Debug.Log("Hit player");
-                transitionObject.GetComponent<screenTransition>().FadeAndLoad(battleScene, transitionDuration);
+                Debug.Log("Hit player, triggering battle");
+                // CombatManager.Instance.SetupCombat(_battleData);
+                _transitionObject.GetComponent<ScreenTransition>().FadeAndLoad(_battleScene, _transitionDuration);
             }
-            else
-            {
-                Debug.DrawRay(sightLineOrigin.transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.red);
-                Debug.Log("Did not hit player, hit object with tag " + hit.transform.gameObject.tag.ToString());
-                Debug.Log(hit.collider.CompareTag("Player"));
-            }
-            
-        }
-        else
-        {
-            Debug.DrawRay(sightLineOrigin.transform.position, transform.TransformDirection(Vector3.forward) * sightLineDistance, Color.white);
-            Debug.Log("Hit nothing");
         }
     }
 }
--- Assets/Scripts/UI/ScreenTransition.cs
+++ Assets/Scripts/UI/ScreenTransition.cs
@@ -3,7 +3,7 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 using System.Collections;
 
-public class screenTransition : MonoBehaviour
+public class ScreenTransition : MonoBehaviour
 {
     public Image faderImage;
     private void Start()
